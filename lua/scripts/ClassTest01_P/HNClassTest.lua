d350b38ec44cbea963ae789e4f8a0c7e82fd929c
Phil Lehmkuhl <phil@lehmkuhl.org>
2025-01-12 01:47:31 +0100

    Adds HNClassTest script

---
+++ lua/scripts/ClassTest01_P/HNClassTest.lua
@@ -0,0 +1,8 @@
+-- Copyright (C) 2024 AnotherlandServer
+--
+-- This software is licensed under the MIT License.
+-- For details, see the LICENSE.md file in the repository.
+
+function SCRIPT:TriggerOnce(player)
+    player:ShowTutorialMessage(2840)
+end
