fe418b3704f0ccd830a3af6fd67dfd65ce35b8c2
Phil Lehmkuhl <phil@lehmkuhl.org>
2025-01-28 01:42:52 +0100

    Implements the requestselectweapon behavior for players

--- lua/scripts/_player.lua
+++ lua/scripts/_player.lua
@@ -4,3 +4,12 @@
 -- For details, see the LICENSE.md file in the repository.
 
 PLAYER.based_on = "_script"
+
+function PLAYER:Init()
+    self:AddBehavior("requestselectweapon", self.Behavior_RequestSelectWeapon)
+end
+
+function PLAYER:Behavior_RequestSelectWeapon(player, id)
+    Log.Debug("Requesting weapon selection for player " .. self.name .. " - " .. id)
+    self:Set("weapon", { id, "00000000-0000-0000-0000-000000000000" })
+end
