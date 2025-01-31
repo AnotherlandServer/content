9e911ca592f997b7523448c7eeb6876326723546
Phil Lehmkuhl <phil@lehmkuhl.org>
2025-01-31 19:04:30 +0100

    Updates copyright

--- lua/scripts/_npc.lua
+++ lua/scripts/_npc.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2024 AnotherlandServer
+-- Copyright (C) 2025 AnotherlandServer
 --
 -- This software is licensed under the MIT License.
 -- For details, see the LICENSE.md file in the repository.
