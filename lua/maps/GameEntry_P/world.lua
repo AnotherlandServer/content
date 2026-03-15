774f23b8453cc381359d24ad461b614a13e732e8
Phil Lehmkuhl <phil@lehmkuhl.org>
2026-03-15 14:18:47 +0100

    Updates copyright.

--- lua/maps/GameEntry_P/world.lua
+++ lua/maps/GameEntry_P/world.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2025 AnotherlandServer
+-- Copyright (C) 2026 AnotherlandServer
 --
 -- This software is licensed under the MIT License.
 -- For details, see the LICENSE.md file in the repository.
